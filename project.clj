c74ea55b568e57eb9d9f59a79bce7e6d98267d80
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-07-27 16:34:00 +0200

    Plugins needed to run and devel this app

--- project.clj
+++ project.clj
@@ -31,6 +31,10 @@
                                                       com.sun.jmx/jmxri]]
                    [org.slf4j/slf4j-log4j12 "1.6.6"]]
     :dev-dependencies [[lein-ring "0.8.10"]]
+    :plugins [[lein-ring "0.8.10"]
+              [codox "0.8.11"]
+              [test2junit "1.1.0"]
+              [lein-cloverage "1.0.6"]]
     :main ^:skip-aot emender-waive.core
     :target-path "target/%s"
     :profiles {:uberjar {:aot :all}})
