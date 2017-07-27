b3953cf7f418b977d06d7a9a1b2ca2046ad4d55a
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-07-27 16:16:13 +0200

    Stub for new Leiningen project

---
+++ test/emender_waive/core_test.clj
@@ -0,0 +1,7 @@
+(ns emender-waive.core-test
+  (:require [clojure.test :refer :all]
+            [emender-waive.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
