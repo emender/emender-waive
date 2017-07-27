e31cc8eb0bd5d9c12ca4ec8ea9e8f9d453abe0c9
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-07-27 16:42:18 +0200

    Updated core module a bit

--- src/emender_waive/core.clj
+++ src/emender_waive/core.clj
@@ -1,7 +1,27 @@
+;
+;  (C) Copyright 2017  Pavel Tisnovsky
+;
+;  All rights reserved. This program and the accompanying materials
+;  are made available under the terms of the Eclipse Public License v1.0
+;  which accompanies this distribution, and is available at
+;  http://www.eclipse.org/legal/epl-v10.html
+;
+;  Contributors:
+;      Pavel Tisnovsky
+;
+
 (ns emender-waive.core
+    "Core module that contains -main function called by Leiningen to start the application."
   (:gen-class))
 
+(require '[ring.adapter.jetty      :as jetty])
+(require '[ring.middleware.params  :as http-params])
+(require '[ring.middleware.cookies :as cookies])
+
+(require '[clojure.tools.cli       :as cli])
+(require '[clojure.tools.logging   :as log])
+
+
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+    "Entry point to the Emender-Jenkins service server."
+    [& args])
