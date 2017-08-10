16fb95d24635c4c2f63a46e86eeb8dbaf1e25222
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-08-10 11:17:32 +0200

    Stub for the tests for the waive module

---
+++ test/emender_waive/waive_test.clj
@@ -0,0 +1,7 @@
+(ns emender-waive.waive-test
+  (:require [clojure.test :refer :all]
+            [emender-waive.waive :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
