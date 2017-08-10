1b5134693450b8550052985cbb2a4f49e5be26ad
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-08-10 11:12:56 +0200

    Stub for the tests for the config-loader module

---
+++ test/emender_waive/config_loader_test.clj
@@ -0,0 +1,7 @@
+(ns emender-waive.config-loader-test
+  (:require [clojure.test :refer :all]
+            [emender-waive.config-loader :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
