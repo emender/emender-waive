06e479f70b81f7559abf894d496821d0625a7b15
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-08-10 11:15:54 +0200

    Stub for the tests for the db-spec module

---
+++ test/emender_waive/db_spec.clj
@@ -0,0 +1,7 @@
+(ns emender-waive.db-spec-test
+  (:require [clojure.test :refer :all]
+            [emender-waive.db-spec :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
