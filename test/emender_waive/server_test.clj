0cc0fbf4d065dbac9e851ea54b7bcf80a6213344
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-09-01 13:23:05 +0200

    Func. existence tests

--- test/emender_waive/server_test.clj
+++ test/emender_waive/server_test.clj
@@ -27,6 +27,59 @@
 ; Tests for functions existence
 ;
 
+(deftest test-return-file-existence
+    "Check that the emender-waive.server/return-file definition exists."
+    (testing "if the emender-waive.server/return-file definition exists."
+        (is (callable? 'emender-waive.server/return-file))))
+
+
+(deftest test-render-front-page-existence
+    "Check that the emender-waive.server/render-front-page definition exists."
+    (testing "if the emender-waive.server/render-front-page definition exists."
+        (is (callable? 'emender-waive.server/render-front-page))))
+
+
+(deftest test-render-error-page-existence
+    "Check that the emender-waive.server/render-error-page definition exists."
+    (testing "if the emender-waive.server/render-error-page definition exists."
+        (is (callable? 'emender-waive.server/render-error-page))))
+
+
+(deftest test-get-hostname-existence
+    "Check that the emender-waive.server/get-hostname definition exists."
+    (testing "if the emender-waive.server/get-hostname definition exists."
+        (is (callable? 'emender-waive.server/get-hostname))))
+
+
+(deftest test-get-api-command-existence
+    "Check that the emender-waive.server/get-api-command definition exists."
+    (testing "if the emender-waive.server/get-api-command definition exists."
+        (is (callable? 'emender-waive.server/get-api-command))))
+
+
+(deftest test-api-call-handler-existence
+    "Check that the emender-waive.server/api-call-handler definition exists."
+    (testing "if the emender-waive.server/api-call-handler definition exists."
+        (is (callable? 'emender-waive.server/api-call-handler))))
+
+
+(deftest test-non-api-call-handler-existence
+    "Check that the emender-waive.server/non-api-call-handler definition exists."
+    (testing "if the emender-waive.server/non-api-call-handler definition exists."
+        (is (callable? 'emender-waive.server/non-api-call-handler))))
+
+
+(deftest test-handler-existence
+    "Check that the emender-waive.server/handler definition exists."
+    (testing "if the emender-waive.server/handler definition exists."
+        (is (callable? 'emender-waive.server/handler))))
+
+
+(deftest test-log-request-existence
+    "Check that the emender-waive.server/log-request definition exists."
+    (testing "if the emender-waive.server/log-request definition exists."
+        (is (callable? 'emender-waive.server/log-request))))
+
 ;
 ; Tests for functions behaviour
 ;
