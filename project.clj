9060294b90fa708d847d09bfd698cc013521a5af
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-03-02 00:32:34 +0100

    Bump up Clojure version

--- project.clj
+++ project.clj
@@ -15,7 +15,7 @@
     :url "http://github.com/emender/emender-waive"
     :license {:name "Eclipse Public License"
               :url "http://www.eclipse.org/legal/epl-v10.html"}
-    :dependencies [[org.clojure/clojure "1.7.0"]
+    :dependencies [[org.clojure/clojure "1.8.0"]
                    [org.clojure/java.jdbc "0.3.5"]
                    [org.clojure/tools.cli "0.3.1"]
                    [org.xerial/sqlite-jdbc "3.7.2"]
